6919719628c179aa06519d4444dde2296effb887
Administrator <alfonso.ricky@maxsol.id>
2021-03-11 12:28:58 +0800

    update

--- storage/cms/cache/3a/77/jules-chevalier-quotes.htm.php
+++ storage/cms/cache/3a/77/jules-chevalier-quotes.htm.php
@@ -1,5 +1,28 @@
 <?php 
-class Cms60498c4624554744245347_c47519071031497e0c9803d73e2624d0Class extends Cms\Classes\PageCode
+use BenFreke\MenuManager\Models\Menu;
+use RainLab\Blog\Models\Category;
+use RainLab\Blog\Models\Post;
+class Cms60499c27e2539272756098_4070512badb773ef6f931aa9d213068fClass extends Cms\Classes\PageCode
 {
+public function onStart() {
+		
+		$query = Post::with('categories')->whereHas('categories', function($query) {
+			return $query->where('slug', 'jules-chevalier-quotes');
+		})->orderBy('published_at', 'desc')->paginate('10');
 
+
+		$this['query'] = $query;
+
+		$menu = Menu::where('title', 'jules chevalier quotes')->where('nest_depth', '>', '1')->first();
+
+		$groupOfMenu = array();
+
+		if(!is_null($menu)){
+		  $groupOfMenu = Menu::where('parent_id',$menu->parent_id)->orderBy('nest_left', 'ASC')->get();
+		}
+
+		$this['menu'] = $menu;
+		
+		$this['groupOfMenu'] = $groupOfMenu;
+	}
 }
